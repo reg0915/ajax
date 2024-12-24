ab73e12e55d80d4a7b743d9a4d52ac2cbd4a4d56
reg <reg8740223@gmail.com>
2024-12-24 16:32:30 +0800

    建立新增用的表單並以modal來顯示

---
+++ app2/create_form.php
@@ -0,0 +1,23 @@
+<!-- Modal -->
+<div class="modal fade" id="CreateModal" tabindex="-1" aria-labelledby="CreateModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="CreateModalLabel">新增學生</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="uni_id" class="form-lable"></label>
+                    <input type="text" class="form-control" id="seat_num">
+
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
+                <button type="button" class="btn btn-primary">儲存</button>
+            </div>
+        </div>
+    </div>
+</div>
